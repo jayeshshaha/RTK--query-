a297b704f20bcbd5cb6f56bd717ae1193efc45ef
Jayesh Shaha <106771749+jayeshshaha@users.noreply.github.com>
2024-02-05 01:45:34 +0530

    rtk done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import PostCard from "./Components/PostCard";
-import { useGetPostsQuery } from "./redux/api"
+import { useGetPostsQuery, useNewPostsMutation } from "./redux/api"
 
 const App = () => {
-     const { isLoading,isError,isSuccess,data,error } =   useGetPostsQuery("");
-     console.log(isLoading,isError,isSuccess,data,error );
+  
+  const { isLoading,isError,isSuccess,data,error } =   useGetPostsQuery("");
+  console.log(isLoading,isError,isSuccess,data,error );
+
+    const [newPost] = useNewPostsMutation();
+
+  const [title, settitle] = useState<string>('');
+  const [views, setviews] = useState<number>(0);
+
+  function handleSubmit(e:React.FormEvent):void{
+       e.preventDefault();
+       const post:Post = {
+        id:String(Math.random() * 1000),
+        title,
+        views
+       }
+       newPost(post);
+       settitle('')
+       setviews(0);
+  }
      
   return (
     <div>
       <h1>My app</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="title"  value={title} onChange={(e)=> settitle(e.target.value)}/>
+        <input type="number" placeholder="body"  value={views} onChange={(e)=> setviews(Number(e.target.value))}/>
+        <button type="submit">Add</button>
+      </form>
       {
         isLoading ? <h1>Loading.....</h1> :(
         data?.map(i => (
--- src/redux/api.ts
+++ src/redux/api.ts
@@ -5,14 +5,24 @@ export const myApi = createApi({
     baseQuery:fetchBaseQuery({
         baseUrl:"http://localhost:3000/"
     }),
+    tagTypes:["Posts"],
     endpoints:(builder) => ({
-            getPosts:builder.query<Post[],string>({query: ()=> "posts"}),
+            getPosts:builder.query<Post[],string>({query: ()=> "posts",providesTags:["Posts"]}),
+            newPosts:builder.mutation<Post,Post>({query:(post)=>({
+                url:"posts",
+                method:"POST",
+                body:post
+            }),
+            invalidatesTags:["Posts"]
+        })
         })
     })
 
+    // tags types basicaly helps use to update data
+    // invalidatesTags:["Posts"] so we this tag name we telling that posts  with tagType has changed
 
 
- export const { useGetPostsQuery } = myApi;
+ export const { useGetPostsQuery, useNewPostsMutation } = myApi;
 
 
 
